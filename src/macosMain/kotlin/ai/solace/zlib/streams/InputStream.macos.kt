d7c285b800dc53bf70ac18b19d3190ff87ed4083
Sydney Renee <sydney@solace.ofharmony.ai>
2025-06-20 00:30:58 -0700

    Refactor: Update Deflate and InputStream classes for improved long handling and error checking

--- src/macosMain/kotlin/ai/solace/zlib/streams/InputStream.macos.kt
+++ src/macosMain/kotlin/ai/solace/zlib/streams/InputStream.macos.kt
@@ -3,8 +3,33 @@ package ai.solace.zlib.streams
 actual abstract class InputStream {
     actual abstract fun read(): Int
     actual open fun read(b: ByteArray): Int = read(b, 0, b.size)
-    actual abstract fun read(b: ByteArray, off: Int, len: Int): Int
+    actual open fun read(b: ByteArray, off: Int, len: Int): Int {
+        if (off < 0 || len < 0 || len > b.size - off) {
+            throw IndexOutOfBoundsException()
+        }
+        if (len == 0) {
+            return 0
+        }
+        var c = read()
+        if (c == -1) {
+            return -1
+        }
+        b[off] = c.toByte()
+        var i = 1
+        try {
+            while (i < len) {
+                c = read()
+                if (c == -1) {
+                    break
+                }
+                b[off + i] = c.toByte()
+                i++
+            }
+        } catch (ee: Exception) {
+        }
+        return i
+    }
     actual open fun skip(n: Long): Long = 0
-    actual abstract fun available(): Int
+    actual open fun available(): Int = 0
     actual open fun close() {}
 }
